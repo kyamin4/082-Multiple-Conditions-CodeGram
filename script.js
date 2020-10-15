3492cdf139e63803eab4dfc68f6fc807c1df5a57
Jonathan Yim <60492176+kyamin4@users.noreply.github.com>
2020-10-14 17:44:19 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,41 @@
+let selectedFilter;
+
+$(".applyFilter").click(function() {
+    let filter = $("input").val();
+    
+    if (filter==="Harlem") {
+        $("img").css("filter", "contrast(115%) brightness(120%)");
+    } else if (filter === "Bushwick") {
+        $("img").css("filter", "contrast(50%) brightness(180%)");
+    }else if (filter === "SOMA") {
+       $("img").css("filter", "grayscale(50%) hue-rotate(10deg)");
+    }else if (filter === "Sunset") {
+        $("img").css("filter", "contrast(115%) hue-rotate(-10deg) saturate(180%)");
+    }else if (filter === "Hidden") {
+        $("img").css("filter", "contrast(150%) saturate(180%) brightness(90%) grayscale(25%)");
+    }
+    
+  // Write if-statements around the following lines.
+    // What is the condition? what should execute if the condition is met?
+    // Let's do the first example together. Below is the code block we want to execute if the user types Harlem in the input. Make sure to uncomment the jQuery code.
+
+        //  $("img").css("filter", "contrast(115%) brightness(120%)");
+
+    // Below is the code block we want to execute if the user types Bushwick in the input. 
+    
+        // $("img").css("filter", "contrast(50%) brightness(180%)");
+    
+    // Below is the code block we want to execute if the user types SOMA in the input. 
+    
+        // $("img").css("filter", "grayscale(50%) hue-rotate(10deg)");
+        
+    // Below is the code block we want to execute if the user types Sunset in the input. 
+    
+        // $("img").css("filter", "contrast(115%) hue-rotate(-10deg) saturate(180%)");
+    
+    
+});
+
+$(".reset").click(function() {
+    $("img").css("filter", "");
+});
